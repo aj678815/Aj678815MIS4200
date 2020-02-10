063797e5e2d0180eec0124d21d235b9822038796
Alexis Jackson <aj678815@ohio.edu>
2020-02-10 14:09:18 -0500

    before delete pet

--- DAL/MIS4200Context.cs
+++ DAL/MIS4200Context.cs
@@ -12,6 +12,7 @@ namespace Aj678815MIS4200.DAL
 
         public MIS4200Context(): base ("name=DefaultConnection")
             {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<MIS4200Context, Aj678815MIS4200.Migrations.MISContext.Configuration>("DefaultConnection"));
 
         }
         public DbSet<Appointment>Appointments { get; set; }
@@ -19,6 +20,10 @@ namespace Aj678815MIS4200.DAL
         public DbSet <Pet> Pets { get; set; }
         public DbSet <AppointmentDetails> AppointmentDetails { get; set; }
 
-
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
+   
 }
